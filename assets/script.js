1ac7330a3ee4913892a3a8830724668f52fbfb7e
didierlp <dddlp94@gmail.com>
2023-05-13 23:27:26 -0600

    mobil

---
+++ assets/script.js
@@ -0,0 +1,18 @@
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${this.dataset.key}"]`);
+    const pad = document.querySelector(`.pad[data-key="${this.dataset.key}"]`);
+    if (!audio) return; // stop the function from running all together
+    audio.currentTime = 0; // rewind to the start
+    audio.play();
+    pad.classList.add("playing");
+  }
+  
+  function removeTransition(e) {
+    if (e.propertyName !== "transform") return;
+    this.classList.remove("playing");
+  }
+  
+  const pads = document.querySelectorAll(".pad");
+  pads.forEach(pad => pad.addEventListener("click", playSound));
+  pads.forEach(pad => pad.addEventListener("transitionend", removeTransition));
+  
